b24dcece6aaf8aabe02e5e132bec85ff068aedaf
Roni <ronibarylko@hotmail.com>
2019-06-30 22:34:23 -0300

    No funciona pero hay que dormir, viejo

--- src/constructor.py
+++ src/constructor.py
@@ -1,21 +1,30 @@
 import random
 import string
 
+STRUCT_MAP = {}
+
 def define_type(val):
     if(val == "int"):
         return str(random.randint(0,100))
     elif(val == "string"):
-        return ''.join(random.sample(string.ascii_lowercase,random.randint(1,30)))
+        return create_random_string()
     elif(val == "float64"):
         return str(random.uniform(0,100))
     elif(val == "bool"):
         return str(bool(random.getrandbits(1)))
     else:
-        return "limpiame sucio"
+        print(STRUCT_MAP)
+        return STRUCT_MAP[val]
 
 def create_array(val):
     res = '[ \n'
-    print(val)
     for num in range(0,random.randint(0,4)):
         res = res + define_type(val) + ", \n"
     return res + define_type(val) + '\n]'
+
+def create_random_string():
+    val = ''.join(random.sample(string.ascii_lowercase,random.randint(1,30)))
+    return "\"" + val + "\""
+
+def register_struct(struct, type_name):
+    STRUCT_MAP[type_name] = struct
--- src/parser.py
+++ src/parser.py
@@ -1,15 +1,17 @@
 import ply.yacc as yacc
 import ply.lex as lex
 import random
-from constructor import define_type, create_array
+from constructor import define_type, create_array, register_struct
 from lexer import tokens
 
 def p_struct_recursive(p):
     'struct_block : type type_name struct struct_definition struct_block '
-    p[0] = '{\n' + p[4] + '\n}' + p[5]
+    register_struct(p[4], p[2])
+    p[0] = '{\n' + p[4] + '\n}'
 
 def p_struct_final(p):
     'struct_block : type type_name struct struct_definition '
+    register_struct(p[4], p[2])
     p[0] = '{\n' + p[4] + '\n}'
 
 def p_attributes(p):
@@ -18,7 +20,7 @@ def p_attributes(p):
 
 def p_type_name(p):
     'type_name : STRING'
-    p[0] = define_type(p[1])
+    p[0] = p[1]
 
 def p_array_type_name(p):
     'array_type_name : STRING'
@@ -30,11 +32,11 @@ def p_array_struct(p):
 
 def p_attributes_definition_recursive(p):
     'attributes_definition : attribute type_name attributes_definition'
-    p[0] = '\"' + p[1] + '\": ' + p[2] + '\n' + p[3]
+    p[0] = '\"' + define_type(p[1]) + '\": ' + p[2] + '\n' + p[3]
 
 def p_attributes_definition_final(p):
     'attributes_definition : attribute type_name'
-    p[0] = '\"' + p[1] + '\": ' + p[2]
+    p[0] = '\"' + define_type(p[1]) + '\": ' + p[2]
 
 def p_attributes_definition_list_recursive(p):
     'attributes_definition : attribute array_struct attributes_definition'
