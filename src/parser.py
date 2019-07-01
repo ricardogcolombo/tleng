e6efe65f559e650dcacfb7a61d68bfd288aa862f
Roni <ronibarylko@hotmail.com>
2019-07-01 18:40:35 -0300

    Refactor parser

--- src/parser.py
+++ src/parser.py
@@ -4,12 +4,14 @@ import random
 from constructor import create_random_string
 from lexer import tokens
 
+### Struct definition
+
 def p_struct_recursive(p):
-    'struct_block : type type_name struct struct_definition struct_block '
+    'struct_block : TYPE type_name STRUCT struct_definition struct_block '
     p[0] = '{\n' + p[4] + '\n}'
 
 def p_struct_final(p):
-    'struct_block : type type_name struct struct_definition '
+    'struct_block : TYPE type_name STRUCT struct_definition '
     p[0] = '{\n' + p[4] + '\n}'
 
 def p_attributes(p):
@@ -20,85 +22,69 @@ def p_type_name(p):
     'type_name : WORD'
     p[0] = p[1]
 
-def p_array_struct(p):
-    'array_struct : array WORD'
-    p[0] = p[2]
-
-def p_array_string_struct(p):
-    'array_struct : array STRING'
-    res = '[ \n'
-    for num in range(0,random.randint(0,4)):
-        res = res + create_random_string() + ", \n"
-    p[0] = res + '\n]'
-
-def p_array_int_struct(p):
-    'array_struct : array INT'
-    res = '[ \n'
-    for num in range(0,random.randint(0,4)):
-        res = res + str(random.randint(0,100)) + ", \n"
-    p[0] = res + '\n]'
-
-def p_array_float_struct(p):
-    'array_struct : array FLOAT64'
-    res = '[ \n'
-    for num in range(0,random.randint(0,4)):
-        res = res + str(random.uniform(0,100)) + ", \n"
-    p[0] = res + '\n]'
-
-def p_array_bool_struct(p):
-    'array_struct : array BOOL'
-    res = '[ \n'
-    for num in range(0,random.randint(0,4)):
-        res = res + str(bool(random.getrandbits(1))) + ", \n"
-    p[0] = res + '\n]'
+### Attributes definition
 
 def p_attributes_definition_recursive(p):
-    'attributes_definition : attribute type_name attributes_definition'
-    p[0] = '\"' + p[1] + '\": ' + p[2] + '\n' + p[3]
+    'attributes_definition : attribute_value attributes_definition'
+    p[0] = p[1] + '\n' + p[2]
 
 def p_attributes_definition_final(p):
-    'attributes_definition : attribute type_name'
-    p[0] = '\"' + p[1] + '\": ' + p[2]
+    'attributes_definition : attribute_value'
+    p[0] = p[1]
 
-def p_attributes_definition_list_recursive(p):
-    'attributes_definition : attribute array_struct attributes_definition'
-    p[0] = '\"' + p[1] + '\": ' + p[2] + '\n' + p[3]
-
-def p_attributes_definition_list_final(p):
-    'attributes_definition : attribute array_struct'
-    p[0] = '\"' + p[1] + '\": ' + p[2]
+### Value definition
 
 def p_attribute(p):
     'attribute : WORD'
     p[0] = p[1]
 
-def p_array(p):
-    'array : ARRAY'
-    p[0] = p[1]
+### STRING
+def p_string_value(p):
+    'attribute_value : attribute STRING'
+    p[0] = '\"' + p[1] + '\": ' + create_random_string()
 
-def p_type_word(p):
-    'type : TYPE'
-    p[0] = p[1]
+def p_array_string_value(p):
+    'attribute_value : attribute ARRAY STRING'
+    res =  '\"' + p[1] + '\": ' + '[ \n'
+    for num in range(0,random.randint(0,4)):
+        res = res + create_random_string() + ", \n"
+    p[0] = res + '\n]'
 
-def p_struct_word(p):
-    'struct : STRUCT'
-    p[0] = p[1]
+### INT
+def p_int_value(p):
+    'attribute_value : attribute INT'
+    p[0] = '\"' + p[1] + '\": ' + str(random.randint(0,100))
 
-def p_string_word(p):
-    'type_name : STRING'
-    p[0] = create_random_string()
+def p_array_int_value(p):
+    'attribute_value : attribute ARRAY INT'
+    res =  '\"' + p[1] + '\": ' + '[ \n'
+    for num in range(0,random.randint(0,4)):
+        res = res + str(random.randint(0,100)) + ", \n"
+    p[0] = res + '\n]'
 
-def p_int_word(p):
-    'type_name : INT'
-    p[0] = str(random.randint(0,100))
+### FLOAT64
+def p_float_value(p):
+    'attribute_value : attribute FLOAT64'
+    p[0] = '\"' + p[1] + '\": ' + str(random.uniform(0,100))
 
-def p_bool_word(p):
-    'type_name : BOOL'
-    p[0] = str(bool(random.getrandbits(1)))
+def p_array_float_value(p):
+    'attribute_value : attribute ARRAY FLOAT64'
+    res =  '\"' + p[1] + '\": ' + '[ \n'
+    for num in range(0,random.randint(0,4)):
+        res = res + str(random.uniform(0,100)) + ", \n"
+    p[0] = res + '\n]'
 
-def p_float_word(p):
-    'type_name : FLOAT64'
-    p[0] = str(random.uniform(0,100))
+### BOOL
+def p_bool_value(p):
+    'attribute_value : attribute BOOL'
+    p[0] = '\"' + p[1] + '\": ' + str(bool(random.getrandbits(1)))
+
+def p_array_bool_value(p):
+    'attribute_value : attribute ARRAY BOOL'
+    res =  '\"' + p[1] + '\": ' + '[ \n'
+    for num in range(0,random.randint(0,4)):
+        res = res + str(bool(random.getrandbits(1))) + ", \n"
+    p[0] = res + '\n]'
 
 # Error rule for syntax errors
 def p_error(p):
